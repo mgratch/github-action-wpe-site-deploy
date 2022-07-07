6b901d9ce4d140b71972deb60ab41f37fe8adcb1
Andrew Matthews <andrew.matthews@wpengine.com>
2022-07-07 12:57:48 -0400

    add e2e test deploy workflow

---
+++ tests/data/scripts/test-plugin.sh
@@ -0,0 +1,20 @@
+# Get the the new plugin version
+AFTER_PLUGIN_VERSION=$(wp plugin get test-plugin | sed -n "/version/p" | cut -f2)
+echo "New test plugin version: $AFTER_PLUGIN_VERSION"
+
+# Revert to backup created by rsync
+rm -rf wp-content/plugins/test-plugin && mv /tmp/test-plugin wp-content/plugins/
+
+# Get the old plugin version
+BEFORE_PLUGIN_VERSION=$(wp plugin get test-plugin | sed -n "/version/p" | cut -f2)
+echo "Old test plugin version: $BEFORE_PLUGIN_VERSION"
+
+# Check that the expected update was made
+if [ "$BEFORE_PLUGIN_VERSION" = "$AFTER_PLUGIN_VERSION" ]; then
+    echo "Failure: Test plugin was not updated!" && exit 1
+else
+    echo "Success: Test plugin successfully updated from $BEFORE_PLUGIN_VERSION to $AFTER_PLUGIN_VERSION!"
+fi
+
+# Cleanup
+rm scripts/test-plugin.sh
