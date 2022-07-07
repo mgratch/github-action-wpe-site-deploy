3697f163c9ecb0d8585d2897dc033e478551218f
Andrew Matthews <andrew.matthews@wpengine.com>
2022-07-07 13:03:28 -0400

    disable change validation for initial deploy

--- tests/data/post-deploy/test-plugin.sh
+++ tests/data/post-deploy/test-plugin.sh
@@ -3,18 +3,18 @@ AFTER_PLUGIN_VERSION=$(wp plugin get test-plugin | sed -n "/version/p" | cut -f2
 echo "New test plugin version: $AFTER_PLUGIN_VERSION"
 
 # Revert to backup created by rsync
-rm -rf wp-content/plugins/test-plugin && mv /tmp/test-plugin wp-content/plugins/
+# rm -rf wp-content/plugins/test-plugin && mv /tmp/test-plugin wp-content/plugins/
 
 # Get the old plugin version
-BEFORE_PLUGIN_VERSION=$(wp plugin get test-plugin | sed -n "/version/p" | cut -f2)
-echo "Old test plugin version: $BEFORE_PLUGIN_VERSION"
+# BEFORE_PLUGIN_VERSION=$(wp plugin get test-plugin | sed -n "/version/p" | cut -f2)
+# echo "Old test plugin version: $BEFORE_PLUGIN_VERSION"
 
 # Check that the expected update was made
-if [ "$BEFORE_PLUGIN_VERSION" = "$AFTER_PLUGIN_VERSION" ]; then
-    echo "Failure: Test plugin was not updated!" && exit 1
-else
-    echo "Success: Test plugin successfully updated from $BEFORE_PLUGIN_VERSION to $AFTER_PLUGIN_VERSION!"
-fi
+# if [ "$BEFORE_PLUGIN_VERSION" = "$AFTER_PLUGIN_VERSION" ]; then
+#     echo "Failure: Test plugin was not updated!" && exit 1
+# else
+#     echo "Success: Test plugin successfully updated from $BEFORE_PLUGIN_VERSION to $AFTER_PLUGIN_VERSION!"
+# fi
 
 # Cleanup
 rm post-deploy/test-plugin.sh
